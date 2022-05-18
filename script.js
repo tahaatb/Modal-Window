2c8a8490bddd8f5c5a96847452d58f1afe41c5d5
Taha Attibbi <tahaatb@gmail.com>
2022-05-18 19:12:57 +0100

    Added javascript to show and remove modal window

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const showModal = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+
+console.log(showModal);
+
+for(let i = 0; i < showModal.length; i++)
+    showModal[i].addEventListener('click', function() {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+
+closeModal.addEventListener('click', function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
